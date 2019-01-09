efea7f7c0c08d26925fe808d0ecf213c9bed4006
Tomaž Erjavec <tomaz.erjavec@ijs.si>
2019-01-09 14:37:23 +0100

    Changes so it can output both syntax part and no-syntaxt part of ssj500k.

--- bin/take-syn.pl
+++ bin/take-syn.pl
@@ -1,9 +1,25 @@
 #!/usr/bin/perl -w
 use utf8;
+$mode = shift;
 binmode STDIN, ":utf8";
 binmode STDOUT,":utf8";
 binmode STDERR,":utf8";
-while (<>){
-    if (/# sent_id\s*=\s*ssj619.3207.11412/) {last}
-    else {print}
+$skip = 1;
+$s_id = 'ssj619.3207.11412'; #First sentence without dependencies
+if ($mode eq 'only') {
+    while (<>){
+	if (/# sent_id\s*=\s*$s_id/) {last}
+	else {print}
+    }
 }
+elsif ($mode eq 'except') {
+    while (<>){
+	if (/# sent_id\s*=\s*$s_id/) {
+	    $skip = 0;
+	    print
+	}
+	elsif ($skip) {}
+	else {print}
+    }
+}
+else {die "Mode is 'only' or 'except'\n"}
